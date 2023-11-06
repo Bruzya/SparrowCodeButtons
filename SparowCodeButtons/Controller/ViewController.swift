2f0618cba90b8f9836b7f68b51135e21a06121c0
Evgenii Mazrukho <mazrukho@gmail.com>
2023-11-06 13:32:18 +0300

    add files, gitignore

---
+++ SparowCodeButtons/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparowCodeButtons
+//
+//  Created by Evgenii Mazrukho on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
